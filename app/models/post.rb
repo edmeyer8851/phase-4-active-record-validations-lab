15a3f0398a20d539a2339e24a93d343c11ddf52f
Eddie Meyer <edmeyer8851@gmail.com>
2023-02-03 11:10:29 -0500

    commit tree b58ceaf95c258975878e38bb500dfbf506a4751d

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :not_true_facts
+
+    def not_true_facts
+        unless title != "True Facts"
+            errors.add(:title, "Title cannot be True Facts")
+        end
+    end
 end
